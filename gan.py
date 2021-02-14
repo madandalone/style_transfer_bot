2a1299ba8f131c3eb2dcf9069f943ce07bef88d2
madandalone <70639131+madandalone@users.noreply.github.com>
2021-02-14 16:03:34 +0300

    Update gan.py

--- gan.py
+++ gan.py
@@ -1,12 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
 
+import os
 
-def transfer(img_path, imsize=512):
-    device = torch.device("cpu")
-    model=torch.load("pretrained_models/monet.pth").to(device)
+
+def transfer(img_path, style, imsize = 256):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = torch.load('./pretrained_models/monet.pth').to(device)
     img = image_loader(img_path, imsize, device)
 
     for p in model.parameters():
@@ -14,15 +18,21 @@ def transfer(img_path, imsize=512):
 
     return model(img)
 
+        
 def image_loader(image_name, imsize, device):
     loader = transforms.Compose([
         transforms.Resize(imsize),
         transforms.CenterCrop(imsize),
         transforms.ToTensor()])
 
-    image = Image.open(image_name)
+    image = Image.open(image_name)  
     image = loader(image).unsqueeze(0)
     return image.to(device, torch.float)
+
+
+def draw_img(img):
+    plt.imshow(np.rollaxis(img.add(1).div(2).cpu().detach()[0].numpy(), 0, 3))
+    plt.show()
 
 
 if __name__ == '__main__':
